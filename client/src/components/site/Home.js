8c8280375d9053b652a149dd3770b37e21f3f907
xtina-lt <xtina_lt@outlook.com>
2022-09-27 16:53:25 -0400

    first commit

---
+++ client/src/components/site/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div>
+            <h2>
+                Come On In....
+            </h2>
+            Tetsting 
+            Tetstingtesting
+            <br/>
+            <img src ="https://media4.giphy.com/media/3o7aCQI45LfvqhURIQ/giphy.gif" alt="bookshelf"/>
+            <br/>
+            The purpose of this application is to:
+            <br/>
+            - Create a full <span className="accent">CRUD</span> app with <span className="accent">React, Express, Node, and MongoDB</span>.
+            <br/>
+            - Include <span className="accent">backend validations</span>. 
+            <br/>
+            - <span className="accent">Refactor, reuse, and recycle</span> React components.
+        </div>
+
+        
+    )
+}
+
+export default Home
